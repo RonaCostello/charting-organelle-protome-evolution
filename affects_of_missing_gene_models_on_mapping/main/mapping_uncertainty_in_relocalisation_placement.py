a8d7220e80c74112e681add6f9b4a8318c815366
Rona Costello <rona.costello@plants.ox.ac.uk>
2019-09-15 19:45:00 +0100

    Add function to set up results dataframe
    For each organelle and then for each node in the species tree, a row is
    added to the dataframe, initialising the counts of relocalisations at
    zero.

--- affects_of_missing_gene_models_on_mapping/main/mapping_uncertainty_in_relocalisation_placement.py
+++ affects_of_missing_gene_models_on_mapping/main/mapping_uncertainty_in_relocalisation_placement.py
@@ -1,6 +1,17 @@
 import pandas as pd
 from ete3 import Tree
 
+def set_up_results_file(tree):
+    organelles = ['Chloroplast', 'Mitochondria', 'Secretory', 'Peroxisome']
+    results_df = pd.DataFrame(columns=['Species_tree_node', 'Organelle', 'old_gains', 'old_losses', 'certain_gains', 'certain_losses', 'new_gains', 'new_losses'])
+    row = 0
+    for organelle in organelles:
+        for node in tree.traverse():
+            results_df.loc[row] = [node.name, organelle] + [0]*6
+            row+=1
+
+    return(results_df)
+
 def topology_between_nodes(tree, node1, node2):
     start_node = tree&node1
     end_node = tree&node2
@@ -16,16 +27,21 @@ def topology_between_nodes(tree, node1, node2):
 
 def main():
     species_tree = Tree("../data/Phytozome10_constrainedTree_rooted_labelled.tree", format=1)
-    
+    results_df = set_up_results_file(species_tree)
+
+    with open('../data/Phyldog_relocalisation_duplication_data.csv') as input_file:
+        for line in input_file:
+            token = line.rstrip().split()
+            print(token)
 
 
-    distance, node_list = topology_between_nodes(species_tree, 'N35', 'N14')
 
 
-    # with open('../data/Phyldog_relocalisation_duplication_data.csv') as input_file:
-    #     for line in input_file:
-    #         token = line.rstrip().split()
-    #         print(token)
+
+
+    # distance, node_list = topology_between_nodes(species_tree, 'N35', 'N28')
+
+
 
 if __name__ == '__main__':
 	main()
