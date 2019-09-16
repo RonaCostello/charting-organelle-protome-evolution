22edcaafe3a1ba659ad95910c057d460215ecfe3
Rona Costello <rona.costello@plants.ox.ac.uk>
2019-09-16 12:28:34 +0100

    Add relocalisation count to the tally in results
    Given a relocalisation and the nodes for which that relocalisation count
    should be split between, this function adds the relocalisation count to
    the correct tally in the results dataframe.
    
    When you call the function you can pick which tally to increment by
    specifying them in a list with the options 'old', 'certain' and 'new'.
    Whether it is a gain or loss is specified by the 'relocalisation_type'
    argument.

--- affects_of_missing_gene_models_on_mapping/main/mapping_uncertainty_in_relocalisation_placement.py
+++ affects_of_missing_gene_models_on_mapping/main/mapping_uncertainty_in_relocalisation_placement.py
@@ -23,16 +23,53 @@ def topology_between_nodes(tree, node1, node2):
         nodes_between.append((start_node.up).name)
         start_node = start_node.up
 
-    return(distance, nodes_between)
+    return(float(distance), nodes_between)
+
+def add_count_to_node(nodes, organelle, df, relocalisation, column_selection):
+    columns_to_update = []
+    cols=[i for i in df.columns if relocalisation in i]
+    for col in cols:
+        if (col.rstrip().rsplit('_'))[0] in column_selection:
+            columns_to_update.append(col)
+
+    increment = 1.0/len(nodes)
+    print(increment)
+    df.loc[(df['Species_tree_node'].isin(nodes)) & (df['Organelle'] == organelle), columns_to_update] += increment
+
+    return(df)
+
 
 def main():
     species_tree = Tree("../data/Phytozome10_constrainedTree_rooted_labelled.tree", format=1)
     results_df = set_up_results_file(species_tree)
 
-    with open('../data/Phyldog_relocalisation_duplication_data.csv') as input_file:
-        for line in input_file:
-            token = line.rstrip().split()
-            print(token)
+    input_df = pd.read_csv('../data/Phyldog_relocalisation_duplication_data.csv')
+
+    cols=[i for i in input_df.columns if 'retention' in i]
+    for col in cols:
+        input_df[col]=pd.to_numeric(input_df[col], errors='coerce')
+    for organelle in ['Chloroplast']:
+        relocalisation_df = input_df.loc[(input_df[f'{organelle[0]}_ingroup_retention'] >= 0.75) & (input_df[f'{organelle[0]}_outgroup_retention'] >= 0.75)]
+
+        for row in relocalisation_df.itertuples(index=False):
+            orthogroup = getattr(row, "orthogroup")
+            relocalisation_type = getattr(row, f"{organelle[0]}_relocalisation")
+            gene_tree = Tree(f'../data/Phyldog_trees/{orthogroup}.locus.tree', format=1)
+
+            gene_tree_node = getattr(row, "orthogroup_tree_node")
+            species_tree_node = getattr(row, "species_tree_node")
+            gene_tree_node_parent = (gene_tree&gene_tree_node).up.name
+            species_tree_node_parent = input_df.loc[(input_df['orthogroup'] == orthogroup) & (input_df['orthogroup_tree_node'] == gene_tree_node), 'species_tree_node'].iloc[0]
+
+            if 'N' not in species_tree_node_parent:
+                results_df = add_count_to_node([species_tree_node_parent], organelle, results_df, relocalisation_type, ['old', 'certain', 'new'])
+
+    results_df.to_csv('test.csv')
+            # print(species_tree_node_parent)
+
+
+
+
 
 
 
