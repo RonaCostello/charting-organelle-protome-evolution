bbca9f9574fa0694c8b34befbd3ff4a59d91bedd
Rona Costello <rona.costello@plants.ox.ac.uk>
2019-09-19 10:26:42 +0100

    Write substitution rate data to csv file

--- substitution_rate_vs_relocalisations/main/change_vs_subrate.py
+++ substitution_rate_vs_relocalisations/main/change_vs_subrate.py
@@ -1,26 +1,75 @@
 from ete3 import Tree
+import pandas as pd
+import csv
+import itertools as it
 
-def topology_between_nodes(tree, node1, node2): # node1 is the node closest to the terminal brances of the tree, with node2 being the deeper node in the tree
-    start_node = tree&node1
-    end_node = tree&node2
+def branch_length_between_nodes(tree, node, parent_node):
+    start_node = tree&node
+    end_node = tree&parent_node
+    common_ancestor = (tree.get_common_ancestor(start_node, end_node))
+    if common_ancestor != end_node and start_node != end_node:
+        raise Exception('node2 must be an ancestor of node1 and not a sister node')
 
-    distance = tree.get_distance(start_node, end_node, topology_only=True)
+    return(tree.get_distance(node, parent_node))
 
-    nodes_between = []
-    for i in range(int(distance)):
-        nodes_between.append((start_node.up).name)
-        start_node = start_node.up
+def is_relocalisation_branch(row, df):
+    evidence_for_relocalisation = False
+    high_scoring_relocalisation = False
 
-    return(nodes_between)
+    for i, col in enumerate(df.columns):
+        if getattr(row, col) == 'gain' or getattr(row, col) == 'loss':
+            evidence_for_relocalisation = True
+            ingroup_retention = float(getattr(row, (df.columns)[i+1]))
+            outgroup_retention = float(getattr(row, (df.columns)[i+2]))
+            if ingroup_retention >= 0.75 and outgroup_retention >= 0.75:
+                high_scoring_relocalisation = True
 
-def branch_length_between_nodes(tree, node1, node2):
-    all_nodes = topology_between_nodes(tree, node1, node2)
-    all_nodes.append(node1)
-    print(all_nodes)
+    return(evidence_for_relocalisation, high_scoring_relocalisation)
 
 def main():
+    results_relocalisation_branches = []
+    results_nonrelocalisation_branches = []
+
     species_tree = Tree("../data/Phytozome10_constrainedTree_rooted_labelled.tree", format=1)
-    branch_length_between_nodes(species_tree, 'N18', 'N18')
+
+    input_df = pd.read_csv('../data/Phyldog_relocalisation_duplication_data.csv')
+    cols=[i for i in input_df.columns if 'retention' in i]
+    for col in cols:
+        input_df[col]=pd.to_numeric(input_df[col], errors='coerce')
+
+    speciation_df = input_df.loc[(input_df['orthogroup_tree_node_event'] == 'Speciation') & (input_df['retention_score'] == 100.0)]
+    for row in speciation_df.itertuples(index=False):
+        orthogroup = getattr(row, "orthogroup")
+        gene_tree = Tree(f'../data/Phyldog_trees/{orthogroup}.locus.tree', format=1)
+
+        gene_tree_node = getattr(row, "orthogroup_tree_node")
+        gene_tree_node_mapped = getattr(row, "species_tree_node")
+
+        gene_tree_node_parent = (gene_tree&gene_tree_node).up.name
+
+        if ((gene_tree&gene_tree_node).up).is_root():
+            continue
+        else:
+            gene_tree_node_parent_mapped = input_df.loc[(input_df['orthogroup'] == orthogroup) & (input_df['orthogroup_tree_node'] == gene_tree_node_parent), 'species_tree_node'].iloc[0]
+
+        if ((species_tree&gene_tree_node_mapped).up) == species_tree&gene_tree_node_parent_mapped:
+            species_tree_distance = float(branch_length_between_nodes(species_tree, gene_tree_node_mapped, gene_tree_node_parent_mapped))
+            gene_tree_distance = float(branch_length_between_nodes(gene_tree, gene_tree_node, gene_tree_node_parent))
+            substitution_rate = gene_tree_distance/species_tree_distance
+            is_branch_reloc, is_branch_retained_reloc = is_relocalisation_branch(row, input_df)
+            if is_branch_reloc == True:
+                if is_branch_retained_reloc == True:
+                    results_relocalisation_branches.append(substitution_rate)
+                else:
+                    continue
+            else:
+                results_nonrelocalisation_branches.append(substitution_rate)
+
+    with open('substitution_rate_on_relocalisation_branches.csv', 'w') as f:
+        csv.writer(f).writerow(["relocalisation_branches", "non-relocalisation_branches"])
+        csv.writer(f).writerows(it.zip_longest(results_relocalisation_branches, results_nonrelocalisation_branches))
+
+
 
 if __name__ == '__main__':
 	main()
